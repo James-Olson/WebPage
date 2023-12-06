99890d0e5ee15708e1edafefeac64064fbe61dc9
Jim Olson <jim.olson.456@gmail.com>
2023-12-06 16:51:10 -0600

    workingondeleverymethods

---
+++ src/main/resources/Javascript/preload.js
@@ -0,0 +1,28 @@
+    document.addEventListener('DOMContentLoaded', function () {
+        var postload = document.querySelectorAll('.postload');
+        for (var j = 0; j < postload.length; j++) {
+            postload[j].style.opacity = 1;
+        }
+
+        function fadeOut(element, duration, delay) {
+            var start = null;
+            function step(timestamp) {
+                if (!start) start = timestamp;
+                var progress = timestamp - start;
+                if (progress < delay) {
+                    requestAnimationFrame(step);
+                    return;
+                }
+                var opacityPercentage = Math.min((progress - delay) / duration, 1);
+                element.style.opacity = 1 - opacityPercentage;
+                if (progress < (delay + duration)) {
+                    requestAnimationFrame(step);
+                }
+            }
+
+            requestAnimationFrame(step);
+        }
+        for (var k = 0; k < postload.length; k++) {
+            fadeOut(postload[k], 1000, 2000);
+        }
+    });
